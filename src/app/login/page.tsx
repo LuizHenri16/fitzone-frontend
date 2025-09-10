62e5b68f4f6b9cd80a66f0fea483b841f7365fdb
LuizHenri16 <lbastos722@gmail.com>
2025-09-10 16:17:43 -0300

    feature: adicionar responsividade em LoginPage

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,8 +2,11 @@ import { LoginForm, Logo } from "@/components"
 
 export default function LoginPage() {
     return (
-        <div className="h-screen w-full flex flex-col justify-between">
-            <Logo />
+        <div className="h-screen w-full mx-auto flex flex-col justify-between
+                        lg:flex-row lg:items-center">
+            <div className="mx-auto my-auto lg:bg-url('images/Background-image.png') lg:bg-no-repeat lg:bg-cover lg:bg-center]">
+                <Logo />
+            </div>
             <LoginForm />
         </div>
     )
