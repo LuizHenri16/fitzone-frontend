e3da784dae0bc543a39d16b7f7e1e48a15504a85
LuizHenri16 <lbastos722@gmail.com>
2025-09-10 17:45:37 -0300

    update/feature: LoginPage com background (pode ser mudado)

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,9 +2,9 @@ import { LoginForm, Logo } from "@/components"
 
 export default function LoginPage() {
     return (
-        <div className="h-screen w-full mx-auto flex flex-col justify-between
+        <div className="bg-[url('/images/Background-image.png')] bg-contain bg-repeat h-screen w-full mx-auto flex flex-col justify-between
                         lg:flex-row lg:items-center">
-            <div className="mx-auto my-auto lg:bg-url('images/Background-image.png') lg:bg-no-repeat lg:bg-cover lg:bg-center]">
+            <div className="mx-auto my-auto">
                 <Logo />
             </div>
             <LoginForm />
