1a2388061807c2c19b02542df775362c4aadce9d
LuizHenri16 <lbastos722@gmail.com>
2025-10-07 14:44:26 -0300

    update: Adicionar ações a botões de da tela de alunos

--- src/app/aluno/page.tsx
+++ src/app/aluno/page.tsx
@@ -1,17 +1,23 @@
 'use client'
 
-import { TableAlunos, Template, Button } from "@/components";
+import { TableAlunos, Template, Button, ClienteModalForm } from "@/components";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function AlunosPage() {
+    const router = useRouter();
+    const [clienteCadastroModalIsOpen, setClienteCadastroModalIsOpen] = useState(false);
+
     return <div>
-        <Template pagename="Alunos">
+        <Template pagename="Alunos" onAbrirModal={() => setClienteCadastroModalIsOpen(true)}>
             <TableAlunos /> 
             <div className="w-full flex flex-col mt-6 gap-5 md:flex-row lg:justify-end">
                 <div className="w-full lg:w-[20rem]">
-                    <Button name="Cadastrar Aluno" theme="brown" type="button" onClick={console.log}/>
+                    <Button name="Cadastrar Aluno" theme="brown" type="button" onClick={() => setClienteCadastroModalIsOpen(true)}/>
+                    <ClienteModalForm isOpen={clienteCadastroModalIsOpen} onClose={()=> setClienteCadastroModalIsOpen(false)} />
                 </div>
                 <div className="w-full lg:w-[20rem]">
-                    <Button name="Voltar" theme="beige" type="button" onClick={console.log}/>
+                    <Button name="Voltar" theme="beige" type="button" onClick={router.back}/>
                 </div>
             </div>
         </Template>
